25ef82ad2de62cb2b35dc8ef587434d8e7df168c
zenggpzqbx <zenggpzqbx@163.com>
2022-09-19 22:26:22 +0800

    修改app.vue，使其可以显示在多个页面间切换

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script setup>
+import {shallowRef} from "vue";
+
+let whichComponents = shallowRef(CesiumLine)
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import CesiumLine from './components/CesiumLine.vue'
+import CesiumLine from './components/CesiumLine.vue'
 import PolylineVolume from './components/PolylineVolume.vue'
+function handleClick(event){
+  console.log(event.target.id)
+  switch (event.target.id) {
+    case 'cesiumLine': whichComponents.value = CesiumLine; break;
+    case 'polylineVolume': whichComponents.value = PolylineVolume ;break;
+  }
+  console.log(whichComponents)
+}
 </script>
 
 <template>
 <!--  <CesiumLine msg="Vite + Vue" />-->
-  <PolylineVolume msg="Customer Components"/>
+  <div class="display_components" @click="handleClick">
+    <button id="cesiumLine">CesiumLine</button>
+    <button id="polylineVolume">PolylineVolume</button>
+  </div>
+  <component :is="whichComponents" :msg="whichComponents"></component>
+<!--  <PolylineVolume msg="Customer Components"/>-->
 </template>
 
 <style scoped>
-
+.display_components{
+  display: flex;
+  flex-direction: row;
+  background-color: aquamarine;
+}
 </style>
