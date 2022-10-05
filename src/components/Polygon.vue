cb8a90e6c786ff8952520ca73fb83207d3a01cf5
zenggp <zenggpzqbx@163.com>
2022-10-05 11:37:50 +0800

    广告牌双面

--- src/components/Polygon.vue
+++ src/components/Polygon.vue
@@ -4,52 +4,53 @@
 </template>
 
 <script setup>
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import initViewer from "../assets/js/initViewer";
 import * as Cesium from "cesium";
 
 let rotation = 0
 let sep = 1
+let material = ref('')
 defineProps({
   msg: String
 })
 onMounted(() => {
   let viewer = initViewer.init()
-  let entities = viewer.entities.add({
+  const cyanPolygon = viewer.entities.add({
     name: "Cyan vertical polygon with per-position heights and outline",
-    //orientation : new Cesium.ImageMaterialProperty({image : '/src/images/Cesium_Logo_Color.jpg', repeat: new Cesium.Cartesian2(1,1)}),
-    wall: {
-      positions: Cesium.Cartesian3.fromDegreesArrayHeights([
-        -115.0,
-        44.0,
-        200000.0,
-        -117.0,
-        44.0,
-        200000.0,
+    polygon: {
+      hierarchy: Cesium.Cartesian3.fromDegreesArrayHeights([
+        -90.0,
+        41.0,
+        0.0,
+        -90.0,
+        41.0,
+        30000.0,
+        -89.0,
+        41.0,
+        30000.0,
+        -89.0,
+        41.0,
+        0.0,
       ]),
-      minimumHeights: [100000.0, 100000.0],
-      //perPositionHeight: true,
-      material: new Cesium.ImageMaterialProperty({image : '/src/images/Cesium_Logo_Color.jpg', repeat: new Cesium.Cartesian2(2,2)})
-      //fill:true,
-      //outline: true,
-      //outlineColor: Cesium.Color.BLACK,
+      material:'/src/images/Cesium_Logo_Color1.jpg',
+      perPositionHeight: true,
+      outline: true,
+      outlineColor: Cesium.Color.BLACK,
     },
-  })
-  // let a = new Cesium.CallbackProperty(function (){
-  //   rotation += sep;
-  //   if(rotation>=360){
-  //     rotation = 0;
-  //   }
-  //   return Cesium.Math.toRadians(rotation)
-  // },false)
-  // console.log(a._callback())
+  });
   setInterval(function (){
-    viewer.zoomTo(entities, new Cesium.HeadingPitchRange(Cesium.Math.toRadians(rotation),-Cesium.Math.toRadians(20), 400000))
+    viewer.zoomTo(cyanPolygon, new Cesium.HeadingPitchRange(Cesium.Math.toRadians(rotation),-Cesium.Math.toRadians(20), 400000))
+    console.log(rotation)
+    if (rotation === 90){
+      cyanPolygon.polygon.material = '/src/images/Cesium_Logo_Color2.jpg'
+    }else if (rotation === 0 || rotation === 270 ){
+      cyanPolygon.polygon.material = '/src/images/Cesium_Logo_Color1.jpg'
+    }
     rotation += sep;
     if(rotation>=360){
       rotation = 0;
     }
-    console.log(rotation)
   },50)
 
 })
